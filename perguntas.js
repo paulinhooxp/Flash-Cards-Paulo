2d83c4ee1567b3b5f6b4cb15670df9b77e1830f0
Paulo Otávio <belica.paulo@escola.pr.gov.br>
2025-09-16 09:22:02 -0300

    16/09

--- perguntas.js
+++ perguntas.js
@@ -1,8 +1,3 @@
-criaCartao(
-    'categoria',
-    'pergunta',
-    'resposta'
-)
 
 criaCartao(
     'geografia',
@@ -12,35 +7,46 @@ criaCartao(
 
 criaCartao(
     'categoria',
-    'pergunta',
-    'resposta'
+    'Quem pintou a obra "Mona Lisa'?,
+    'Leonardo Da Vinci'
+
+    criaCartao(
+    '',
+    'Em que ano o homem pisou na Lua pela primeira vez?',
+    '1969'
 
     criaCartao(
     'categoria',
-    'pergunta',
-    'resposta'
+    'Qual é o elemento químico representado pela sigla "O"?',
+    'Oxigênio'
 
     criaCartao(
     'categoria',
-    'pergunta',
-    'resposta'
+    'Quantos estados tem o Brasil?',
+    ' 26 estados (mais o Distrito Federal'
+
+    criaCartao(
+    'geografia',
+    'Qual é a capital do Canadá?',
+    'ottawa'
 
     criaCartao(
     'categoria',
-    'pergunta',
-    'resposta'
+    'Quem escreveu a peça "Romeu e Julieta"?',
+    'William Shakespeare'
 
     criaCartao(
     'categoria',
-    'pergunta',
-    'resposta'
+    'Qual é o rio mais extenso do mundo?',
+    ' Rio Nilo (há debates com o Amazonas, mas o Nilo é tradicionalmente aceito)'
 
-    criaCartao(
+    CriaCartao(
     'categoria',
-    'pergunta',
-    'resposta'
+    'Em que continente fica o deserto do Saara?',
+    'África'
 
-    criaCartao(
+    CriaCartao(
     'categoria',
-    'pergunta',
-    'resposta'
+    'Qual é o idioma mais falado no mundo atualmente (como língua nativa)?',
+    'Mandarim (Chinês)'
+
