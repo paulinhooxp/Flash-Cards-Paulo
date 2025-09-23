45c38f3964731caebdb696796ce2cb62549f7aa0
Paulo Otávio <belica.paulo@escola.pr.gov.br>
2025-09-23 10:33:45 -0300

    23/09

--- app.js
+++ app.js
@@ -18,8 +18,9 @@ function criaCartao(categoria, pergunta, resposta) {
 
     function viraCartao(){
         respostaEstaVisivel=!respostaEstaVisivel
+        cartao.classList.toggle('active', respostaEstaVisivel)
     }
-     
+     cartao.addEventListener('click',viraCartao)
     
     container.appendChild(cartao)
     
